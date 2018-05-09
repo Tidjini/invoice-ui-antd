e4ca48d93d202b35c8fb52e2bc76e2bda12579c8
Tidjini_Messaoudi <dy.31@hotmail.fr>
2018-05-09 10:57:05 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { DatePicker, Button } from "antd";
+
+function App() {
+  return (
+    <div style={{ margin: 100 }}>
+      <h1>AntDesign Demo</h1>
+      <hr />
+      <br />
+      <DatePicker style={{ backgroundColor: "#CCC" }} />
+      <Button type="primary" ghost>
+        Primary
+      </Button>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ theme.js
@@ -0,0 +1,7 @@
+module.exports = () => {
+  return {
+    "primary-color": "#FFD95F",
+    "link-color": "#1DA57A",
+    "border-radius-base": "2px"
+  };
+};
